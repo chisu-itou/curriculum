02a9fd11d48bfa4f0976d5c8f5454f0b09aa04b0
chisu-itou <79260159+chisu-itou@users.noreply.github.com>
2021-10-09 23:41:07 +0900

    課題提出テスト
    Task.javaの作成

---
+++ JavaStudy/1_1/Task1_1.java
@@ -0,0 +1,14 @@
+public class Task1_1{
+    public static void main(String[] args){
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        //double型を宣言して、「hogeDouble」という変数名をつけて、0.2で初期化する。
+        double hogeDouble = 0.2;
+        hogeInt = 100;
+        hogeString = "合格";
+        System.out.println(hogeInt);
+        //変数 hogeString の値を表示
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+    }
+}
