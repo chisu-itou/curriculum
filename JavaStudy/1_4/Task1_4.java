7d2c4bd7331c277937bddd1d5a353c59ceecb2c1
chisu-itou <79260159+chisu-itou@users.noreply.github.com>
2021-10-12 23:48:08 +0900

    課題提出テスト
    Task1_4.javaの作成

--- JavaStudy/1_4/Task1_4.java
+++ JavaStudy/1_4/Task1_4.java
@@ -38,19 +38,19 @@ public class Task1_4 {
         // ② 「name」の値が「USER_NAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
         //      定数を使用して「 ログイン成功です 」と出力してください。
 
-        if(name == USER_NAME && pass == USER_PASSWORD){
+        if(name.equals(USER_NAME)&&pass.equals(USER_PASSWORD)){
             System.out.println(CONST_MSG_SUCCESS);
 
         // ③ 「USER_NAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
 
-        }else if(name == USER_NAME){
+        }else if(name.equals(USER_NAME)){
             System.out.println(CONST_MSG_ERROR_PASS);
 
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
 
-        }else if(pass == USER_PASSWORD){
+        }else if(pass.equals(USER_PASSWORD)){
             System.out.println(CONST_MSG_ERROR_NAME);
 
         // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
